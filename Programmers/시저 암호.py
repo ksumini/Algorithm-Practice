90cc5ae93876a2c0b9279b4610e3dbc77ac85fa7
SUMIN KIM <70088803+ksumini@users.noreply.github.com>
2021-10-16 19:42:54 +0900

    Update 시저 암호.py

--- Programmers/시저 암호.py
+++ Programmers/시저 암호.py
@@ -1,3 +1,7 @@
+# 소문자와 대문자 확인
+# isupper(), islower() 사용
+# z 이후 값들은 알파벳 총길이(26)으로 나누었을때 나머지로 해결
+
 def solution(s, n):    
     alpha_lower = 'abcdefghijklmnopqrstuvwxyz'
     alpha_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
