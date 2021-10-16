32503bffece55a99df62c61164d7d20255dfd176
SUMIN KIM <70088803+ksumini@users.noreply.github.com>
2021-10-17 00:57:30 +0900

    Update 시저 암호.py

--- Programmers/시저 암호.py
+++ Programmers/시저 암호.py
@@ -17,3 +17,18 @@ def solution(s, n):
             end = alpha_lower.find(i) + n
             ans += alpha_lower[end % 26]
     return ans
+
+
+# 아스키 코드를 활용한 풀이
+# ord(문자): 하나의 문자를 인자로 받고 해당 문자에 해당하는 유니코드 정수를 반환
+# chr(정수): 하나의 정수를 인자로 받고 해당 정수에 해당하는 유니코드 문자를 반환
+
+def solution(s, n): 
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i] = chr(ord('A') + (ord(s[i]) - ord('A') + n) % 26) # 대문자 'A'를 기준으로 n만큼 이동한 후 해당하는 알파벳 찾기
+        elif s[i].islower():
+            s[i] = chr(ord('a') + (ord(s[i]) - ord('a') + n) % 26) # 소문자 'a'를 기준으로 n만큼 이동한 후 해당하는 알파벳 
+
+    print("".join(s))
