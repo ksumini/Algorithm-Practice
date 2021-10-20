dd2668caa81369af96b9eec52c1cb69018202b4c
SUMIN KIM <70088803+ksumini@users.noreply.github.com>
2021-10-20 22:45:19 +0900

    Update 11478.서로 다른 부분 문자열의 개수.py

--- Baekjoon/11478.서로 다른 부분 문자열의 개수.py
+++ Baekjoon/11478.서로 다른 부분 문자열의 개수.py
@@ -1,8 +1,9 @@
 # 572 ms
 import sys
-s = sys.stdin.readline().strip()
+s = sys.stdin.readline().strip() # 문자열 입력 받기
+
 ans = []
 for i in range(len(s)):
-    for j in range(i+1, len(s)+1):
-        ans.append(s[i:j])
-print(len(set(ans)))
+    for j in range(i+1, len(s)+1): 
+        ans.append(s[i:j]) # 경우의 수 구하기
+print(len(set(ans))) # 중복 제거후 개수
