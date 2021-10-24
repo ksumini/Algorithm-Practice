db90e792e18e1f43e51900cbdc32c3e11c6107a1
SUMIN KIM <70088803+ksumini@users.noreply.github.com>
2021-10-24 21:04:17 +0900

    Create 숫자 문자열과 영단어.py

---
+++ Programmers/숫자 문자열과 영단어.py
@@ -0,0 +1,6 @@
+def solution(s):
+    numbers = {'0':'zero', '1':'one', '2':'two', '3':'three', '4':'four', '5':'five', '6':'six', '7':'seven', '8':'eight', '9':'nine'}
+    for key, value in numbers.items():
+        if value in s:
+            s = s.replace(value, key)
+    return int(s)
