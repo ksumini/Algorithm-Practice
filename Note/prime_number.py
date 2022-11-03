1076b9e130afaeaeb24aea5ba7c6ee3691224fa0
SUMIN KIM <70088803+ksumini@users.noreply.github.com>
2022-11-03 23:26:20 +0900

    Create prime_number.py

---
+++ Note/prime_number.py
@@ -0,0 +1,9 @@
+# 소수 판별 함수-> 시간복잡도 O(X^1/2)
+def is_prime_number(x):
+    # 2부터 x의 제곱근까지의 모든 수를 확인하며
+    for i in range(2, int(x ** (1/2))+1):
+        print(i)
+        # x가 해당 수로 나누어떨어진다면
+        if x % i == 0:
+            return False # 소수가 아님
+    return True # 소수임
