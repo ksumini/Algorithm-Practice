d4fd77e3b167d589c11a05c91b4365965475cd3e
SUMIN KIM <70088803+ksumini@users.noreply.github.com>
2022-11-04 13:18:24 +0900

    Update Prime Number.py

--- Note/Prime Number.py
+++ Note/Prime Number.py
@@ -2,7 +2,6 @@
 def is_prime_number(x):
     # 2부터 x의 제곱근까지의 모든 수를 확인하며
     for i in range(2, int(x ** (1/2))+1):
-        print(i)
         # x가 해당 수로 나누어떨어진다면
         if x % i == 0:
             return False # 소수가 아님
